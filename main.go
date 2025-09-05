50290c35d3139806532c14a6e0e06d9ba6590989
lyydsheep <2230561977@qq.com>
2025-09-05 16:57:51 +0800

    feat: Implement solution for longest common increasing subsequence

--- main.go
+++ main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
-	"coding-questions/algorithm"
-	"fmt"
+	"bufio"
+	. "fmt"
+	"os"
 )
 
+var (
+	w *bufio.Writer
+	r *bufio.Reader
+)
+
+func init() {
+	w, r = bufio.NewWriter(os.Stdout), bufio.NewReader(os.Stdin)
+}
+
+func solve(a, b []int) {
+	n := len(a)
+	f := make([][]int, n+1)
+	for i := range f {
+		f[i] = make([]int, n+1)
+	}
+
+	for i := 1; i <= n; i++ {
+		maxv := 0
+		for j := 1; j <= n; j++ {
+			// 没有 a[i]
+			f[i][j] = f[i-1][j]
+
+			// 有 a[i]
+			if a[i-1] == b[j-1] {
+				f[i][j] = max(f[i][j], maxv)
+			}
+
+			maxv = max(maxv+1, f[i-1][j])
+		}
+	}
+}
+
 func main() {
-	defer algorithm.Writer.Flush()
+	defer w.Flush()
 	var n int
-	fmt.Fscan(algorithm.Reader, &n)
-	fmt.Fprint(algorithm.Writer, n)
+	Fscan(r, &n)
+
+	a, b := make([]int, n), make([]int, n)
+	for i := range a {
+		Fscan(r, &a[i])
+	}
+	for i := range b {
+		Fscan(r, &b[i])
+	}
+
+	solve(a, b)
 }
