1b14da8ee123a7720c838137238084002d31d5da
lyydsheep <2230561977@qq.com>
2025-09-14 21:53:20 +0800

    feat(algorithm): 单调队列优化多重背包问题

--- main.go
+++ main.go
@@ -6,50 +6,52 @@ import (
 	"os"
 )
 
+// ACM 输入输出
 var (
-	w *bufio.Writer
 	r *bufio.Reader
+	w *bufio.Writer
 )
 
 func init() {
-	w, r = bufio.NewWriter(os.Stdout), bufio.NewReader(os.Stdin)
-}
-
-func solve(a, b []int) {
-	n := len(a)
-	f := make([][]int, n+1)
-	for i := range f {
-		f[i] = make([]int, n+1)
-	}
-
-	for i := 1; i <= n; i++ {
-		maxv := 0
-		for j := 1; j <= n; j++ {
-			// 没有 a[i]
-			f[i][j] = f[i-1][j]
-
-			// 有 a[i]
-			if a[i-1] == b[j-1] {
-				f[i][j] = max(f[i][j], maxv)
-			}
-
-			maxv = max(maxv+1, f[i-1][j])
-		}
-	}
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
 }
 
 func main() {
 	defer w.Flush()
-	var n int
-	Fscan(r, &n)
+	var n, m int
+	Fscan(r, &n, &m)
+	// 滚动数组
+	f, g := make([]int, m), make([]int, m+1)
+	q := make([]int, n)
 
-	a, b := make([]int, n), make([]int, n)
-	for i := range a {
-		Fscan(r, &a[i])
-	}
-	for i := range b {
-		Fscan(r, &b[i])
+	for i := 0; i < n; i++ {
+		var v, w, s int
+		Fscan(r, &v, &w, &s)
+
+		copy(g, f)
+		// 枚举余数
+		for j := 0; j < v; j++ {
+			// 单调队列
+			hh, tt := 0, -1
+			for k := j; k <= m; k += v {
+				// 移除队头
+				if tt >= hh && q[hh] < k-s*v {
+					hh++
+				}
+
+				// 取出窗口内最大值
+				f[k] = max(f[k], g[q[hh]]+(k-q[hh])/v*w)
+
+				// 加入窗口
+				for tt >= hh && g[q[tt]]-(q[tt]-j)/v*w <= f[k]-(k-j)/v*w {
+					tt--
+				}
+				tt++
+				q[tt] = k
+			}
+		}
 	}
 
-	solve(a, b)
+	Fprintln(w, f[m])
 }
